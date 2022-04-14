56f379f20dce4f3c5f59b113e290d122c3c6497e
ylcdx <yann.lecommandoux@gmail.com>
2022-04-14 16:42:10 +0200

    Initial blog

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import PostsView from "@/views/PostsView.vue";
+import PostView from "@/views/PostView.vue";
+import AuthorView from "@/views/AuthorView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "posts",
+      component: PostsView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/post/:id",
+      name: "post",
+      component: PostView,
+    },
+    {
+      path: "/authors",
+      name: "authors",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      component: () => import("@/views/AuthorsView.vue"),
+    },
+    {
+      path: "/author/:username",
+      name: "author",
+      component: AuthorView,
+    },
+  ],
+});
 
-export default router
+export default router;
